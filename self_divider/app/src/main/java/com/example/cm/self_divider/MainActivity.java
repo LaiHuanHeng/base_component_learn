9e5aaecd5481908a79e40ac943789cc4097b8e53
buder <chenpeng3@cmcm.com>
2018-09-11 17:09:29 +0800

    self_define_divider

---
+++ self_divider/app/src/main/java/com/example/cm/self_divider/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.cm.self_divider;
+
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private MyAdapter mAdapter;
+    private LinearLayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mRecyclerView = (RecyclerView)findViewById(R.id.my_recycler_view);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        //方式一：
+        mRecyclerView.addItemDecoration(new RecycleViewDivider(getApplicationContext(), LinearLayoutManager.VERTICAL, 20, 60, 60, getApplicationContext().getResources().getColor(R.color.colorAccent)));
+
+
+        //方式二：简单方式
+//        DividerItemDecoration divider = new DividerItemDecoration(this,DividerItemDecoration.VERTICAL);
+//        divider.setDrawable(ContextCompat.getDrawable(this,R.drawable.divider_mileage));
+//        mRecyclerView.addItemDecoration(divider);
+
+        mRecyclerView.setHasFixedSize(true);
+        mAdapter = new MyAdapter(getDatas());
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    private ArrayList<String> getDatas() {
+        ArrayList<String> data = new ArrayList<>();
+        String temp = " item";
+        for(int i = 0; i < 20; i++) {
+            data.add(i + temp);
+        }
+        return data;
+    }
+
+    public static class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+        public ArrayList<String> datas = null;
+        public MyAdapter(ArrayList<String> datas) {
+            this.datas = datas;
+        }
+        //创建新View，被LayoutManager所调用
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+            View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item,viewGroup,false);
+            ViewHolder vh = new ViewHolder(view);
+            return vh;
+        }
+        //将数据与界面进行绑定的操作
+        @Override
+        public void onBindViewHolder(ViewHolder viewHolder, int position) {
+            viewHolder.mTextView.setText(datas.get(position));
+        }
+        //获取数据的数量
+        @Override
+        public int getItemCount() {
+            return datas.size();
+        }
+        //自定义的ViewHolder，持有每个Item的的所有界面元素
+        public static class ViewHolder extends RecyclerView.ViewHolder {
+            public TextView mTextView;
+            public ViewHolder(View view){
+                super(view);
+                mTextView = (TextView) view.findViewById(R.id.text);
+            }
+        }
+    }
+}
